f37476972ece6bb268a7ee068a10861652aed3a8
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2024-05-24 15:43:35 +0530

    l++

---
+++ Intro Programs/arrAvg.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class arrAvg {
+    static int avgarr(int arr[]){
+        int sum = 0;
+        for(int i = 0; i< arr.length; i++){
+            sum = sum + arr[i];
+        }
+    
+        return sum / arr.length;
+    }
+    public static void main(String[] args) {
+        Scanner key = new Scanner(System.in);
+        int arrlength = key.nextInt();
+        int[] arr = new int[arrlength];
+        for(int i=0; i<arrlength;i++){
+            arr[i] = key.nextInt();
+        }
+       System.out.print(avgarr(arr)); 
+    }
+}
