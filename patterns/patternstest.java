3a5748ddc96f14bcf70690795f2e4ba524aa0453
Abhay Shah <61384622+abhayshah0305@users.noreply.github.com>
2024-05-31 22:31:11 +0530

    update

--- patterns/patternstest.java
+++ patterns/patternstest.java
@@ -2,7 +2,7 @@ package patterns;
 
 public class patternstest {
     public static void main(String[] args) {
-        pattern3(5);
+        pattern17(5);
     }
     static void pattern1(int n){
         for(int i = 1; i <= n; i++){
@@ -11,6 +11,7 @@ public class patternstest {
             }
             System.out.println();
         }
+        
     }
     static void pattern2(int n){
         for(int i = 1; i <= n; i++){
@@ -28,4 +29,44 @@ public class patternstest {
             System.out.println();
         }
     }
+    static void pattern30(int n){
+        
+        for(int row = 1; row <= n; row++){
+
+            for(int spaces = 0; spaces < (n-row); spaces++){
+                System.out.print("  ");
+            }
+
+            for(int col = row; col >= 1; col--){
+                System.out.print(col + " ");
+            }
+
+            for(int col = 2; col <= row; col++){
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+
+        }
+    }
+    static void pattern17(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+
+            int c = row > n ? 2 * n - row: row;
+
+            for (int space = 0; space < n-c; space++) {
+                System.out.print("  ");
+            }
+
+            for (int col = c; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
 }
